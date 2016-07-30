8fd45f94ed164fd40d5ae1cc698a65e3fedbc71f
David Morant <dmorant95@gmail.com>
2016-07-30 15:56:24 -0400

    Add Application class w/scheduler setup

---
+++ app/src/main/java/com/candidcold/cacophony/CacophonyApplication.java
@@ -0,0 +1,35 @@
+package com.candidcold.cacophony;
+
+import android.app.AlarmManager;
+import android.app.Application;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
+import android.support.v7.app.AppCompatDelegate;
+
+public class CacophonyApplication extends Application {
+
+    private JobScheduler scheduler;
+
+    static {
+        AppCompatDelegate.setDefaultNightMode(
+                AppCompatDelegate.MODE_NIGHT_AUTO);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        scheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        JobInfo.Builder builder =
+                new JobInfo.Builder(1, new ComponentName(getPackageName(), RingtoneJobService.class.getName()));
+
+        builder.setPersisted(true);
+        builder.setPeriodic(AlarmManager.INTERVAL_DAY);
+        // TODO: Add more as needed
+
+        scheduler.schedule(builder.build());
+
+    }
+}
