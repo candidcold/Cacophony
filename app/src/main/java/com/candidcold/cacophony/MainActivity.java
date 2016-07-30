49f87de798d204ebde363a975715da6646c237a6
David Morant <dmorant95@gmail.com>
2016-07-30 15:49:25 -0400

    Migrate notification logic to JobService

--- app/src/main/java/com/candidcold/cacophony/MainActivity.java
+++ app/src/main/java/com/candidcold/cacophony/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Setup fragment transaction
         if (savedInstanceState == null) {
-            com.candidcold.cacophony.RingtoneFragment ringtoneFragment = new com.candidcold.cacophony.RingtoneFragment();
+            RingtoneFragment ringtoneFragment = new com.candidcold.cacophony.RingtoneFragment();
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
             ft.add(R.id.container, ringtoneFragment);
--- app/src/main/java/com/candidcold/cacophony/RingtoneJobService.java
+++ app/src/main/java/com/candidcold/cacophony/RingtoneJobService.java
@@ -1,7 +1,14 @@
 package com.candidcold.cacophony;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+import java.util.Random;
 
 public class RingtoneJobService extends JobService {
 
@@ -13,5 +20,42 @@ public class RingtoneJobService extends JobService {
     @Override
     public boolean onStopJob(JobParameters jobParameters) {
         return false;
+    }
+
+    private int getRandomNumber(int upperBound) {
+        return new Random().nextInt(upperBound);
+    }
+
+    private void buildNotification(Context context, String title, String body) {
+        // Send a notification to the user that the ringtone has been changed
+        int color = context.getResources().getColor(R.color.accent);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.drawable.ic_queue_music_white_24dp)
+                        .setColor(color)
+                        .setContentTitle(title)
+                        .setContentText(body)
+                        .setAutoCancel(true);
+
+        // Handle when the notification is clicked to take it to the app's main activity.
+        Intent resultIntent = new Intent(context, MainActivity.class);
+
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        context,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        int mNotificationId = 001;
+        // Gets an instance of the NotificationManager service
+        NotificationManager mNotifyMgr =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        // Builds the notification and issues it.
+        mNotifyMgr.notify(mNotificationId, mBuilder.build());
     }
 }
