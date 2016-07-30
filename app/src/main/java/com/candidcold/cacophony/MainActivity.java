1a76bd9945b86f61230f145c124bd3a460d22ef9
David Morant <dmorant95@gmail.com>
2016-07-30 15:39:46 -0400

    Update to AppCompatActivity

--- app/src/main/java/com/candidcold/cacophony/MainActivity.java
+++ app/src/main/java/com/candidcold/cacophony/MainActivity.java
@@ -3,10 +3,10 @@ package com.candidcold.cacophony;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     Toolbar appBar;
 
